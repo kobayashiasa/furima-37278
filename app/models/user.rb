ff3f6fc12c1ef52154b4351b4849f1eb57a96f20
kobayashiasa <kobayashiasa@gmail.com>
2022-03-15 12:41:44 +0900

    ユーザー登録の修正２

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_date, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
@@ -15,7 +11,7 @@ class User < ApplicationRecord
     validates :first_name
     validates :last_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ン]+\z/, message: 'には全角カタカナを使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: 'には全角カタカナを使用してください' } do
     validates :first_name_kana
     validates :last_name_kana
   end
