3a7861f2b65a1342f8e295dbaec2e07f6d4e19b6
kobayashiasa <kobayashiasa@gmail.com>
2022-03-20 14:49:21 +0900

    viewファイル修正プルダウン表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.order("created_at DESC")
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :item_price, :item_info, :category_id, :item_condition_id, :who_pays_shipping_id, :prefecture_id, :shipping_date_id )
   end
 end
