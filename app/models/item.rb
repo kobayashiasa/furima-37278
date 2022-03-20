0e596c434cb5dc4096c9c4541a72353f01a1b46b
kobayashiasa <kobayashiasa@gmail.com>
2022-03-20 12:27:25 +0900

    itemモデルのアソシエーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :item_condition
+  belongs_to :who_pays_shipping
+  belongs_to :prefecture
+  belongs_to :shipping_date
 end
