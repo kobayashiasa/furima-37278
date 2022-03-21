2a7be2320866d599d16b72e87dff58f2bd643ca4
kobayashiasa <kobayashiasa@gmail.com>
2022-03-21 17:22:14 +0900

    itemモデル単体テストコード作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,8 +8,8 @@ class Item < ApplicationRecord
   belongs_to :who_pays_shipping
   belongs_to :prefecture
   belongs_to :shipping_date
-
+  
   validates :item_name, :item_price, :item_info, :image, presence: true
   validates :category_id, :item_condition_id, :who_pays_shipping_id, :prefecture_id, :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :item_price, numericality: {only_integer: true, greater_than: 299, less_than: 10000000}
+  validates :item_price, numericality: { greater_than_or_equal_to: 300, less_than: 9999999 , message:'is out of setting range'}
 end
