cf02ad7739cb32127f98ce22e727c8a1ced7cefd
kobayashiasa <kobayashiasa@gmail.com>
2022-04-10 16:06:26 +0900

    テーブル作成、アソシエーションの追加

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  has_one :shipping_info
   validates :price, presence: true
 end
--- db/migrate/20220410065340_create_shipping_infos.rb
+++ db/migrate/20220410065340_create_shipping_infos.rb
@@ -1,6 +1,13 @@
 class CreateShippingInfos < ActiveRecord::Migration[6.0]
   def change
     create_table :shipping_infos do |t|
+      t.string  :zip_code,             null: false
+      t.integer :prefecture_id,        null: false
+      t.string  :city,                 null: false
+      t.string  :address ,             null: false
+      t.string  :building
+      t.integer :phone,                null: false
+      t.references :order,             null: false, foreign_key: true
 
       t.timestamps
     end
